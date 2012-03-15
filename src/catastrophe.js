8c9af384ee103f8c373d696cfd9609c650100b7d
Spencer Tipping <spencer@spencertipping.com>
2012-03-15 14:31:29 -0600

    Tracer design

--- src/catastrophe.js
+++ src/catastrophe.js
@@ -1,3 +1,5 @@
-caterwaul.module( 'catastrophe' ,function($) {catastrophe=function(options) {;
-return function(f,environment) {;
-return f} } } ) ;
+caterwaul.module( 'catastrophe' ,function($) { (function( ) {var statics= {caterwaul:$.deglobalize() } ,tracer_for=function(options) {;
+return(function( ) {var default_options= {mocks: { } ,environment: { } ,pre_trace:true,trace:true} ,settings=$.merge( { } ,default_options,options) ;
+return(trace) } ) .call(this) } ;
+return($.merge( (catastrophe=function(options) {;
+return tracer_for(options) } ) ,statics) ) } ) .call(this) } ) ;
